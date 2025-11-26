e7de2ed128accc3cad14f9c824fd668616bccd3c
Farah YOUNES <imgettingbetter99@gmail.com>
2025-11-26 11:55:19 +0100

    création tables

---
+++ script_projet_SPA.sql
@@ -0,0 +1,116 @@
+CREATE TABLE refuge (
+    refuge_id NUMBER(3) PRIMARY KEY,
+    nom VARCHAR(50),
+    adresse VARCHAR(100),
+    nb_animaux NUMBER(3)
+);
+
+CREATE TABLE animal (
+    animal_id NUMBER(3) PRIMARY KEY,
+    nom VARCHAR(20),
+    espece VARCHAR(30),
+    race VARCHAR(30),
+    age NUMBER(2),
+    sexe CHAR(1), -- mettre que 'F' ou 'M'
+    date_arrivee_refuge DATE,
+    date_depart_refuge DATE,
+    taille NUMBER(3),
+    caractere VARCHAR(30),
+    etat_sante VARCHAR(10), -- 'sain' ou 'malade'
+    statut_adoption VARCHAR(20),
+    rstr_alim VARCHAR(20),
+    traitement CHAR(3), -- 'oui' ou 'non'
+    refuge_id NUMBER(3),
+    CONSTRAINT fk_refuge_id_animal
+    FOREIGN KEY (refuge_id) REFERENCES refuge(refuge_id)
+);
+
+CREATE TABLE enclos (
+    enclos_id NUMBER(3) PRIMARY KEY,
+    capacite NUMBER(2),
+    type_enclos VARCHAR(20),
+    occupation NUMBER(2),
+    refuge_id NUMBER(3),
+    CONSTRAINT fk_refuge_id_enclos
+    FOREIGN KEY (refuge_id) REFERENCES refuge(refuge_id)
+);
+
+CREATE TABLE adoptant (
+    adoptant_id NUMBER(4) PRIMARY KEY,
+    nom VARCHAR(20),
+    age NUMBER(2),
+    espace_ext CHAR(3), -- 'oui' ou 'non'
+    profil_cherche VARCHAR(30),
+    mail VARCHAR(50),
+    nb_animaux_possedes NUMBER(2)
+);
+
+CREATE TABLE benevole (
+    bnv_id NUMBER(3) PRIMARY KEY,
+    nom VARCHAR(20),
+    fonction VARCHAR(30),
+    age NUMBER(2),
+    mail VARCHAR(50),
+    date_arrivee DATE
+);
+
+CREATE TABLE mission (
+    mission_id NUMBER(3) PRIMARY KEY,
+    description VARCHAR(30),
+    statut VARCHAR(20) -- 'realisee' ou 'non realisee'
+);
+
+CREATE TABLE fournisseur (
+    frs_id NUMBER(3) PRIMARY KEY,
+    nom VARCHAR(20),
+    type_fourniture VARCHAR(30)
+);
+
+CREATE TABLE alimentation (
+    alim_id NUMBER(3) PRIMARY KEY,
+    type_alim VARCHAR(30),
+    qte_dispo NUMBER(3)
+);
+
+CREATE TABLE realise (
+    mission_id NUMBER(3),
+    bnv_id NUMBER(3),
+    animal_id NUMBER(3),
+    deb_mission TIMESTAMP,
+    fin_mission TIMESTAMP,
+    PRIMARY KEY (mission_id, bnv_id, animal_id),
+    CONSTRAINT fk_mission_id_realise FOREIGN KEY (mission_id) REFERENCES mission(mission_id),
+    CONSTRAINT fk_bnv_id_realise FOREIGN KEY (bnv_id) REFERENCES benevole(bnv_id),
+    CONSTRAINT fk_animal_id_realise FOREIGN KEY (animal_id) REFERENCES animal(animal_id)
+);
+
+CREATE TABLE fournis (
+    frs_id NUMBER(3),
+    alim_id NUMBER(3),
+    qte_fourniture NUMBER(3),
+    prix_fourniture NUMBER(7,2),
+    date_livraison DATE,
+    PRIMARY KEY (frs_id, alim_id),
+    CONSTRAINT fk_frs_id_fournis FOREIGN KEY (frs_id) REFERENCES fournisseur(frs_id),
+    CONSTRAINT fk_alim_id_fournis FOREIGN KEY (alim_id) REFERENCES alimentation(alim_id)
+);
+
+CREATE TABLE mange (
+    animal_id NUMBER(3),
+    alim_id NUMBER(3),
+    qte_conso NUMBER(2),
+    PRIMARY KEY (animal_id, alim_id),
+    CONSTRAINT fk_animal_id_mange FOREIGN KEY (animal_id) REFERENCES animal(animal_id),
+    CONSTRAINT fk_alim_id_mange FOREIGN KEY (alim_id) REFERENCES alimentation(alim_id)
+);
+
+CREATE TABLE garde (
+    enclos_id NUMBER(3),
+    animal_id NUMBER(3),
+    deb_sejour DATE,
+    fin_sejour DATE,
+    PRIMARY KEY (enclos_id, animal_id),
+    CONSTRAINT fk_enclos_id_garde FOREIGN KEY (enclos_id) REFERENCES enclos(enclos_id),
+    CONSTRAINT fk_animal_id_garde FOREIGN KEY (animal_id) REFERENCES animal(animal_id)
+);
+
