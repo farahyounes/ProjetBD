cac7591a67c808ce7f912b3967d5655beef4f556
Alyssamrl <alyssa.morellon@gmail.com>
2025-12-15 22:49:50 +0100

    triggers 1,2,3

--- script_projet_SPA.sql
+++ script_projet_SPA.sql
@@ -325,13 +325,52 @@ AND A.etat_sante = 'fragile';
 ------Vues------
 
 --V1
-CREATE VIEW Vue_animaux_disponibles
+CREATE VIEW vue_animaux_dispo
 SELECT animal_id
 FROM animal
 WHERE statut_adoption = 'disponible';
 
 --V2
-CREATE VIEW Vue_adoption
+CREATE VIEW vue_adoption
 SELECT animal_id, adoptant_id, date_depart_refuge
 FROM animal
 WHERE statut_adoption = 'adopté';
+
+
+
+
+------Triggers------
+
+--T1
+ALTER TABLE adoptant
+ADD CONSTRAINT chk_espace_ext
+CHECK (espace_ext = 'oui');
+
+--T2
+ALTER TABLE adoptant
+ADD CONSTRAINT chk_nb_animaux_max
+CHECK (nb_animaux_possedes <= 4);
+
+--T3
+CREATE OR REPLACE TRIGGER trg_rstr_alim
+BEFORE INSERT OR UPDATE ON mange
+DECLARE
+    v_restriction VARCHAR2(30);
+    v_type_alim  VARCHAR2(30);
+BEGIN
+
+    SELECT rstr_alim
+    INTO v_restriction
+    FROM animal
+    WHERE animal_id = :NEW.animal_id;
+
+    SELECT type_alim
+    INTO v_type_alim
+    FROM alimentation
+    WHERE alim_id = :NEW.alim_id;
+
+    IF v_restriction IS NOT NULL AND v_restriction = v_type_alim THEN
+        RAISE_APPLICATION_ERROR(-20002, 'Cet aliment est incompatible avec la restriction alimentaire de l’animal');
+    END IF;
+END;
+/
