ec6dc14666f09f422753b31f13fa6023cf09e16f
mounag5 <guehairia.mouna5@gmail.com>
2025-12-16 14:37:06 +0100

    triggers 4,5,6

--- script_projet_SPA.sql
+++ script_projet_SPA.sql
@@ -391,3 +391,55 @@ BEGIN
     END IF;
 END;
 /
+
+--T4
+CREATE OR REPLACE TRIGGER trg_espace_enclos
+BEFORE INSERT OR UPDATE ON garde
+
+FOR EACH ROW
+DECLARE
+    v_especes_existantes VARCHAR(30);
+    v_especes_nouvelles VARCHAR(30);
+BEGIN
+   SELECT espece
+   INTO v_especes_nouvelles
+   FROM animal a 
+   WHERE a.animal_id=:NEW.animal_id;
+
+   SELECT a.espece 
+   INTO v_especes_existantes
+   FROM animal a, garde g 
+   WHERE g.animal_id=a.animal_id
+   AND g.enclos_id= :NEW.enclos_id
+   AND ROWNUM=1;
+
+   IF v_especes_existantes <> v_especes_nouvelles THEN --Le <> pr verif si diff
+      RAISE_APPLICATION_ERROR(-20001, 'On ne peut pas mélanger différentes espèces dans un même enclos.');
+   END IF;
+
+ EXCEPTION
+ WHEN NO_DATA_FOUND THEN
+   NULL;
+END;
+/
+   
+--T5
+CREATE trg_age_min_animal
+BEFORE UPDATE OF statut_adoption ON animal
+
+FOR EACH ROW
+BEGIN 
+   IF :NEW.statut_adoption='adopte' 
+      AND :NEW.age <0.25 THEN
+
+    RAISE_APPLICATION_ERROR(-20001, 'Un animal peut être adopté à partir de 3 mois');
+   END IF;
+
+END;
+/
+
+--T6
+ALTER TABLE benevole
+ADD CONSTRAINT chk_benevole_age
+CHECK(age>=18);
+
